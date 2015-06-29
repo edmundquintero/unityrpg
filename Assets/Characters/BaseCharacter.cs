8d39570c38cdfdf96697e3763a290d002f8c00dc
Edmund Quintero <edmundquintero@gmail.com>
2015-06-29 09:22:54 -0400

    Added getters/setters in baseCharacter. Started baseMage

--- Assets/Characters/BaseCharacter.cs
+++ Assets/Characters/BaseCharacter.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class BaseCharacter{
 	private string characterName;
 	private string characterDescription;
+
 	private int stamina;
 	private int intelligence;
 	private int endourance;
+
 	private int experience;
 	private int level;
 
@@ -14,10 +16,29 @@ public class BaseCharacter{
 		get{ return characterName; }
 		set{ characterName = value; }
 	}
-
 	public string CharacterDescription{
 		get{ return characterDescription; }
 		set{ characterDescription = value; }
+	}
+	public int Stamina{
+		get{ return stamina; }
+		set{ stamina = value; }
+	}
+	public int Intelligence{
+		get{ return intelligence; }
+		set{ intelligence = value; }
+	}
+	public int Endourance{
+		get{ return endourance; }
+		set{ endourance = value; }
+	}
+	public int Experience{
+		get{ return experience; }
+		set{ experience = value; }
+	}
+	public int Level{
+		get{ return level; }
+		set{ level = value; }
 	}
 
 }
---
+++ Assets/Characters/baseMageCharacter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class baseMageCharacter : BaseCharacter {
+
+	public void MageClass(){
+		CharacterName = "Mage";
+		CharacterDescription = "Magic weilding individual.";
+
+		Intelligence = 10;
+		Endourance = 5;
+		Stamina = 8;
+
+	}
+
+}
