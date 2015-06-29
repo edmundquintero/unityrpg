e1a8fe74026fcc534311e4c71ef20614ffa21462
Edmund Quintero <edmundquintero@gmail.com>
2015-06-28 22:38:13 -0400

    Initial commit

---
+++ Assets/Characters/BaseCharacter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseCharacter{
+	private string characterName;
+	private string characterDescription;
+	private int stamina;
+	private int intelligence;
+	private int endourance;
+
+}
