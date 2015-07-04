e6e3597abc4d1cbe4e1b830fcf647d963c35fa9c
Edmund Quintero <edmundquintero@gmail.com>
2015-07-04 18:44:25 -0400

    Adding initial GUI

---
+++ Assets/PlayerGUI/CreatePlayerGUI.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatePlayerGUI : MonoBehaviour {
+
+	public enum CreateAPlayerStates{
+	  CLASSSELECTION,
+	  STATALLOCATION,
+	  FINALSETUP
+	}
+
+	private DisplayCreatePlayerFunctions displayFunctions = new DisplayCreatePlayerFunctions();
+	public static CreateAPlayerStates currentState;
+
+	// Use this for initialization
+	void Start () {
+		currentState = CreateAPlayerStates.CLASSSELECTION;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (currentState) {
+		case(CreateAPlayerStates.CLASSSELECTION):
+			break;
+		case(CreateAPlayerStates.STATALLOCATION):
+			break;
+		case(CreateAPlayerStates.FINALSETUP):
+			break;
+		}
+	
+	}
+
+	void OnGUI(){
+		if (currentState == CreateAPlayerStates.CLASSSELECTION) {
+			//displat class selection
+			displayFunctions.DisplayClassSelections();
+		}
+		if (currentState == CreateAPlayerStates.STATALLOCATION) {
+			//displat class selection
+			displayFunctions.DisplayStatAllocation();
+		}
+		if (currentState == CreateAPlayerStates.FINALSETUP) {
+			//displat class selection
+			displayFunctions.DisplayFinalSetup();
+		}
+	}
+}
---
+++ Assets/PlayerGUI/DisplayCreatePlayerFunctions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayCreatePlayerFunctions {
+
+	private int classSelection;
+	private string[] classSelectionNames = new string[] {"Mage", "Warrior"};
+
+	public void DisplayClassSelections(){
+		classSelection = GUI.SelectionGrid (new Rect (50, 50, 250, 300), classSelection, classSelectionNames, 2);
+	}
+	public void DisplayStatAllocation(){
+		
+	}
+	public void DisplayFinalSetup(){
+		
+	}
+}
